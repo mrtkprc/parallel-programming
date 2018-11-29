13f83a028bba81afa51b7e98ea61dbe85cdba051
Mert KÖPRÜCÜ <mrtkprc@gmail.com>
2018-11-29 18:12:04 +0300

    Children Process created files

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <math.h>
 
 #define OPENING_FILE "qr_basic_3.dat"
 
@@ -18,6 +19,8 @@
 int readFile(FILE **file,int *N,double **matrix);
 void writeMatrix(const double *matrix, const int *N);
 void divideMatrix(double **matrix,const int *N,double divider);
+int openFile(FILE **file,const char *opening_mode,const char *file_name);
+void write2File(FILE **file);
 
 //
 void divideMatrix(double **matrix,const int *N,double divider)
@@ -26,7 +29,7 @@ void divideMatrix(double **matrix,const int *N,double divider)
     double val;
     for(i = 0; i < *N ;i++)
         for(k = 0; k < *N ;k++)
-            *((*matrix)+((i+1)*k)) = (*((*matrix)+((i+1)*k))) / divider;
+            *((*matrix)+(*N)*i+k) = (*((*matrix)+(*N)*i+k)) / divider;
 }
 
 void writeMatrix(const double *matrix, const int *N)
@@ -37,8 +40,8 @@ void writeMatrix(const double *matrix, const int *N)
     {
         for(k = 0; k < *N ;k++)
         {
-            val = *(matrix+((i+1)*k));
-            printf("%.2f ",val);
+            val = *(matrix+(i*(*N)+k));
+            printf("%f ",val);
         }
         printf("\n");
     }
@@ -73,41 +76,68 @@ int readFile(FILE **file,int *N,double **matrix)
 				strncpy(foundNewLine,"",1); // new line character is cleared
             strcpy(tmpLine,(const char*)line);
             line_val = atof(tmpLine);
-
-            *(*matrix+(lineOrder-1)) = line_val;
+            *((*matrix)+lineOrder-1) = line_val;
             lineOrder++;
         }
     }
     return 0;
 }
 
+int openFile(FILE **file,const char *opening_mode,const char *file_name)
+{
+    *file = fopen(file_name,"r");
+    if(*file == NULL)
+    {
+        printf("File %s failed\n",file_name);
+        return -1;
+    }
+    printf("File %s opened\n",file_name);
+    return 0;
+}
 
+void write2File(FILE **file)
+{
+    printf("file writing starting\n");
+    fprintf(*file,"mert\n");
+    fflush(*file);
+    printf("file writing starting\n");
+
+}
 int main (int argc, char *argv[])
 {
     int i;
+    char tmpLine[250];
     int my_order = -1;
     FILE *file;
+    FILE *file_writing;
     int N=0;
     double *matrix;
     pid_t f;
     pid_t *child_creation_list;
     int shm_children_list_id; //shared memory children list id
+    double *each_child_row;
+    double val;
 
-    file = fopen(OPENING_FILE,"r");
+    openFile(&file,"r",OPENING_FILE);
+    
     if(!file)
     {
 		printf("Input file cannot be opened\n");
 		exit(1);
     }
     readFile(&file,&N,&matrix);
+    fclose(file);
+
     //writeMatrix(matrix,&N);
+
     divideMatrix(&matrix,&N,255.0);
+    //writeMatrix(matrix,&N);
 
     shm_children_list_id = shmget(SHMCHILDLIST,sizeof(pid_t)*N,0700|IPC_CREAT);
     child_creation_list =(pid_t *)shmat(shm_children_list_id,0,0);
 
-    //child_creation_list = (pid_t*)malloc(sizeof(pid_t) * N);
-    
+    each_child_row = (double *)malloc(sizeof(double)*N);
+
     for (i = 0; i < N; i++)
 	{
 		f = fork();
@@ -126,7 +156,7 @@ int main (int argc, char *argv[])
     if(f!=0) // Parent Proces
 	{
         wait(0);
-        printf("Parent Process ID: %u\n",getpid());
+        printf("The End\n");
         shmdt(child_creation_list);
         shmctl(shm_children_list_id,IPC_RMID,0);
     }
@@ -135,12 +165,28 @@ int main (int argc, char *argv[])
         printf("My Process ID: %u and Parent ID : %u\n",getpid(),getppid());
         for (i = 0; i < N; i++)
             if (child_creation_list[i] == getpid())
-                my_order = i+1;
+                my_order = i;
         
-        printf("My order :%d\n",my_order);
+        for (i = 0; i < N; i++)
+            *(each_child_row + i) = pow(matrix[my_order*N+i],0.5);
+
+        snprintf(tmpLine,250,"%d",my_order+1);
+        strcat(tmpLine,".dat");
+        printf("file name: %s\n",tmpLine);
+        file_writing = fopen((const char*)tmpLine,"w");
+        if (file_writing == NULL) 
+        {
+            perror("fopen");
+            exit(1);
+        }
+        
+        for (i = 0; i < N; i++)
+            fprintf(file_writing,"%f\n",*(each_child_row + i));
+        
+        fflush(file_writing);
+        fclose(file_writing);
+
         sleep(1);
+        wait(0);
     }
-
-
-
 }
