c1a0720efb094a7b37f9380a066301ea8053c938
Mert KÖPRÜCÜ <mrtkprc@gmail.com>
2018-11-29 22:11:02 +0300

    Basic Multi Thread Structure Created

---
+++ multi_thread.c
@@ -0,0 +1,239 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+#include <pthread.h>
+#include <time.h>
+
+double *matrix;
+
+
+int readFile(FILE **file,int *N,double **matrix);
+void writeMatrix(const double *matrix, const int *N);
+void divideMatrix(double **matrix,const int *N,double divider);
+int openFile(FILE **file,const char *opening_mode,const char *file_name);
+
+//
+void divideMatrix(double **matrix,const int *N,double divider)
+{
+    int i,k;
+    double val;
+    for(i = 0; i < *N ;i++)
+        for(k = 0; k < *N ;k++)
+            *((*matrix)+(*N)*i+k) = (*((*matrix)+(*N)*i+k)) / divider;
+}
+
+void writeMatrix(const double *matrix, const int *N)
+{
+    int i,k;
+    double val;
+    for(i = 0; i < *N ;i++)
+    {
+        for(k = 0; k < *N ;k++)
+        {
+            val = *(matrix+(i*(*N)+k));
+            printf("%f ",val);
+        }
+        printf("\n");
+    }
+}
+
+int readFile(FILE **file,int *N,double **matrix)
+{
+    char *line,tmpLine[256];
+    int lineOrder = 0;
+	int order = 0;
+	size_t len = 0;
+	int read; 
+    char *foundNewLine;
+    double line_val;
+    int N_number;
+    
+
+    while((read = getline(&line,&len,*file)) != -1)
+	{
+		if(lineOrder == 0) 
+		{
+            N_number = atoi(line);
+			lineOrder++;
+            *matrix = (double *)malloc(sizeof(double) * N_number * N_number);
+            memset(*matrix,0,sizeof(double)*N_number*N_number);
+            *N = N_number;
+		}
+        else
+        {
+            foundNewLine = strstr(line,"\n");
+            if(foundNewLine != NULL)
+				strncpy(foundNewLine,"",1); // new line character is cleared
+            strcpy(tmpLine,(const char*)line);
+            line_val = atof(tmpLine);
+            *((*matrix)+lineOrder-1) = line_val;
+            lineOrder++;
+        }
+    }
+    return 0;
+}
+
+int openFile(FILE **file,const char *opening_mode,const char *file_name)
+{
+    *file = fopen(file_name,"r");
+    if(*file == NULL)
+    {
+        printf("File %s failed\n",file_name);
+        return -1;
+    }
+    //printf("File %s opened\n",file_name);
+    return 0;
+}
+
+
+void *each_child_thread(void *arg)
+{
+    int thread_order = (int)arg;
+    printf("Thread val: %d\n",thread_order);
+    pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[])
+{
+    int i,k;
+    clock_t start,end;
+    char *line;
+    int read;
+    size_t len;
+    char tmpLine[250];
+    int my_order = -1;
+    FILE *file;
+    FILE *file_writing;
+    int N=0;
+    
+    double *each_child_row;
+    double val;
+    pthread_t *thread_list;
+    pthread_attr_t attr;
+    int rc;
+    void *status;
+
+    if(argc != 2)
+    {
+        printf("Please, use following format\n");
+        printf("<./exe_name> <source dat file>\n");
+        return EXIT_FAILURE;
+    }
+
+    start = clock();
+    
+    openFile(&file,"r",argv[1]);
+    
+    if(!file)
+    {
+		printf("Input file cannot be opened\n");
+		exit(1);
+    }
+    readFile(&file,&N,&matrix);
+    fclose(file);
+
+    //writeMatrix(matrix,&N);
+
+    divideMatrix(&matrix,&N,255.0);
+    //writeMatrix(matrix,&N);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+    thread_list = (pthread_t*)malloc(sizeof(pthread_t)* N);
+    printf("Thread Creating\n");
+    
+    for (i = 0; i < N; i++)
+    {
+        rc = pthread_create((thread_list+i),&attr,each_child_thread,(void *)i);
+        if(rc)
+        {
+            printf("Error in creating thread\n");
+            return EXIT_FAILURE;
+        }
+    }
+    pthread_attr_destroy(&attr);
+    for (i = 0; i < N; i++)
+    {
+        rc = pthread_join(*(thread_list+i),&status);
+        if(rc)
+        {
+            printf("Error in join thread\n");
+            return EXIT_FAILURE;
+        }
+    }
+    printf("Threads Created\n");
+    pthread_exit(NULL);
+    
+
+    /*
+    if(f!=0) // Parent Proces
+	{
+        wait(0);
+        file_writing = fopen("result_multi_proc.dat","w");
+        if (file_writing == NULL) 
+        {
+            perror("fopen");
+            exit(1);
+        }
+        for (i = 0; i < N; i++)
+	    {
+            snprintf(tmpLine,250,"%d",i+1);
+            strcat(tmpLine,".dat");
+            openFile(&file,"r",tmpLine);
+           
+            while((read = getline(&line,&len,file)) != -1)
+	        {
+                if(!strcmp(line,"\n"))
+                    break;
+                fprintf(file_writing,"%s",line);
+                //printf("Each file :%s");
+                fflush(file_writing);
+            }
+            fclose(file);
+        }
+        fclose(file_writing);
+        end = clock();
+        printf("The Elapsed Time: %f\n",(((double) (end - start)) / CLOCKS_PER_SEC));
+        
+    }
+    else
+    {
+        //printf("My Process ID: %u and Parent ID : %u\n",getpid(),getppid());
+        for (i = 0; i < N; i++)
+            if (child_creation_list[i] == getpid())
+                my_order = i;
+        
+        for (i = 0; i < N; i++)
+            *(each_child_row + i) = pow(matrix[my_order*N+i],0.5);
+
+        snprintf(tmpLine,250,"%d",my_order+1);
+        strcat(tmpLine,".dat");
+        //printf("file name: %s\n",tmpLine);
+        file_writing = fopen((const char*)tmpLine,"w");
+        if (file_writing == NULL) 
+        {
+            perror("fopen");
+            exit(1);
+        }
+        
+        for (i = 0; i < N; i++)
+            fprintf(file_writing,"%f\n",*(each_child_row + i));
+        
+        fflush(file_writing);
+        fclose(file_writing);
+
+        sleep(1);
+        wait(0);
+    }
+    */
+   return 0;
+}
