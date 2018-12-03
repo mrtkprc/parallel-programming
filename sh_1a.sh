6a730a0cc63fee57ad8668e29ef06cd03d7a43eb
Mert KÖPRÜCÜ <mrtkprc@gmail.com>
2018-12-03 14:26:53 +0300

    All Test Done

---
+++ sh_1a.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_64x64_grayscale.dat &&
+sleep 2 &&
+
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_128x128_grayscale.dat &&
+sleep 2 &&
+
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2 &&
+./hw1a qr_code_512x512_grayscale.dat &&
+sleep 2
